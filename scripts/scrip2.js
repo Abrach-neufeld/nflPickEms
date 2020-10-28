57286a5d8a8d1341b73611c247854c83da8e5e6c
Avi Brach-Neufeld <abrachne@bates.edu>
2020-10-28 08:30:44 -0500

    factored out data

--- scripts/scrip2.js
+++ scripts/scrip2.js
@@ -79,37 +79,12 @@ async function getWinProb(url) {
 
 
 
-async function run_me(){
+async function run_me(urlList,picks){
   var rows = [];
   var gm;
-  const avisPicks = ["A","A","H","H","H","H","A","H","H","A","H","H","H","H"]
-  const williePicks = ["H","H","H","H","H","H","H","H","H","H","H","H","H","H"]
-  const joshsPicks =["H","H","H","H","H","H","H","H","H","H","H","H","H","H"]
-  const alexPicks = ["H","H","H","H","H","H","H","H","H","H","H","H","H","H"]
-  const chuckPicks = ["H","H","H","H","H","H","H","H","H","H","H","H","H","H"]
-  const mikePicks = ["H","H","H","H","H","H","H","H","H","H","H","H","H","H"]
-  const zachPicks = ["H","H","H","H","H","H","H","H","H","H","H","H","H","H"]
-  const jasperPicks = ["H","H","H","H","H","H","H","H","H","H","H","H","H","H"]
-  const jacobPicks = ["H","H","H","H","H","H","H","H","H","H","H","H","H","H"]
 
 
-  const picks = [avisPicks,williePicks,joshsPicks,alexPicks,chuckPicks,mikePicks,zachPicks,jasperPicks,jacobPicks]
 
-  urlList=["https://www.espn.com/nfl/game/_/gameId/401220314",
-  "https://www.espn.com/nfl/game/_/gameId/401220115",
-  "https://www.espn.com/nfl/game/_/gameId/401220158",
-  "https://www.espn.com/nfl/game/_/gameId/401220169",
-  "https://www.espn.com/nfl/game/_/gameId/401220288",
-  "https://www.espn.com/nfl/game/_/gameId/401220292",
-  "https://www.espn.com/nfl/game/_/gameId/401220222",
-  "https://www.espn.com/nfl/game/_/gameId/401220127",
-  "https://www.espn.com/nfl/game/_/gameId/401220148",
-  "https://www.espn.com/nfl/game/_/gameId/401220212",
-  "https://www.espn.com/nfl/game/_/gameId/401220277",
-  "https://www.espn.com/nfl/game/_/gameId/401220361",
-  "https://www.espn.com/nfl/game/_/gameId/401220258",
-  "https://www.espn.com/nfl/game/_/gameId/401220257"
-]
   for (i in urlList){
 
     gm = await getWinProb(urlList[i])
@@ -228,4 +203,4 @@ var projected=[0,0,0,0,0,0,0,0,0]
   return rows
 
 }
-rows = run_me();
+rows = run_me(urlList,picks);
