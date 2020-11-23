9a3ae89be529ffa3b3c7cc264d441e2726a588af
Josh Oettinger <joshkyle98@gmail.com>
2020-11-22 19:23:39 -0600

    Added picks form, built backend

---
+++ src/PickMaker.js
@@ -0,0 +1,237 @@
+import React from 'react'
+import {Container, Row, Col, Card, CardDeck, Image, Form, Button} from 'react-bootstrap'
+import axios from 'axios'
+import {DragDropContext, Draggable, Droppable} from 'react-beautiful-dnd'
+import './PickMaker.css'
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import {withRouter} from 'react-router-dom'
+
+class PickMaker extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      loadingGames: true,
+      games: [],
+      picks: {},
+      pickOrder: [],
+      name: '',
+      week: null,
+      players: []
+    }
+  }
+
+  componentDidMount() {
+    const promises = []
+    promises.push(this.loadGameData())
+    const db = firebase.firestore()
+    promises.push(db.collection('players').get().then((snapshot) => {
+      this.setState({
+        players: snapshot.docs.map(doc => doc.id)
+      })
+    }))
+    Promise.all(promises).then(() => this.setState({loadingGames: false}))
+  }
+
+  submitPicks = () => {
+    if (this.state.pickOrder.length !== this.state.games.length) {
+      alert('You must make a pick for every game.')
+    } else {
+      const db = firebase.firestore()
+      const gamesCollection = db.collection('weeks').doc(this.state.week.toString()).collection('games')
+      const promises = []
+      for (let i = 0; i < this.state.pickOrder.length; i++) {
+        const pick = this.state.pickOrder[i]
+        gamesCollection.doc(pick.gameID).set({gameID: pick.gameID})
+        promises.push(gamesCollection.doc(pick.gameID).collection('picks').doc(this.state.name).set({
+          pick: pick.team.abbr,
+          weight: this.state.pickOrder.length - i
+        }))
+      }
+      Promise.all(promises).then(() => this.props.history.push('/'))
+    }
+  }
+
+  loadGameData = async () => {
+    const response = await axios.get('https://site.api.espn.com/apis/site/v2/sports/football/nfl/scoreboard')
+    const games = response.data.events
+    this.setState({
+      week: response.data.week.number
+    })
+    const gameData = []
+    for (const game of games) {
+      const teams = game.competitions[0].competitors
+      const teamData = []
+      for (let team of teams) {
+        team = team.team
+        teamData.push({
+          name: team.name,
+          abbr: team.abbreviation,
+          color: team.color,
+          logo: team.logo
+        })
+      }
+      gameData.push({
+        id: game.id,
+        teams: teamData
+      })
+    }
+    this.setState({ games: gameData })
+  }
+
+  reorder = (list, startIndex, endIndex) => {
+    const result = Array.from(list);
+    const [removed] = result.splice(startIndex, 1);
+    result.splice(endIndex, 0, removed);
+
+    return result;
+  }
+
+  onDragEnd = (result) => {
+    // dropped outside the list
+    if (!result.destination) {
+      return
+    }
+
+    const pickOrder = this.reorder(
+      this.state.pickOrder,
+      result.source.index,
+      result.destination.index
+    )
+
+    this.setState({
+      pickOrder
+    })
+  }
+
+  setPick = (gameID, team, opposingTeam) => {
+    const newOrder = this.state.pickOrder.filter(item => item.gameID !== gameID)
+    newOrder.push({
+      gameID,
+      team,
+      opposingTeam
+    })
+    this.setState({
+      picks: {
+        ...this.state.picks,
+        [gameID]: team.abbr
+      },
+      pickOrder: newOrder
+    })
+  }
+
+  renderTeamCard = (team, gameID, opposingTeam) => {
+    return (
+      <Card border={this.state.picks[gameID] === team.abbr ? 'success' : ''} className='teamCard mb-3' onClick={() => this.setPick(gameID, team, opposingTeam)}>
+        <Card.Body>
+          <Row className='cardContent'>
+            <Col><Image src={team.logo} className='teamLogo'/></Col>
+            <Col><Card.Title>{team.abbr}</Card.Title></Col>
+          </Row>
+        </Card.Body>
+      </Card>
+    )
+  }
+
+  renderPickForm = () => {
+    const rows = []
+    for (const game of this.state.games) {
+      rows.push(
+        <Row>
+          <CardDeck style={{alignItems: 'center'}}>
+            {this.renderTeamCard(game.teams[0], game.id, game.teams[1])}
+            <h3>vs</h3>
+            {this.renderTeamCard(game.teams[1], game.id, game.teams[0])}
+          </CardDeck>
+        </Row>
+      )
+    }
+    return rows
+  }
+
+  renderOrderer = () => {
+    return (
+      <DragDropContext onDragEnd={this.onDragEnd}>
+        <Droppable droppableId='droppable'>
+          {(provided, snapshot) => (
+            <div
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+            >
+              {this.state.pickOrder.map((item, index) => (
+                <Draggable key={item.gameID} draggableId={item.gameID} index={index}>
+                  {(provided, snapshot) => (
+                    <Card
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                      className='mb-3'
+                    >
+                      <Card.Body>
+                        <Row className='cardContent'>
+                          <Col><Image src={item.team.logo} className='teamLogo'/></Col>
+                          <Col><Card.Title>{item.team.abbr} over {item.opposingTeam.abbr}</Card.Title></Col>
+                          <Col><Image src={item.opposingTeam.logo} className='teamLogo'/></Col>
+                        </Row>
+                      </Card.Body>
+                    </Card>
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
+    )
+  }
+
+  renderForm = () => {
+    return (
+      <Container>
+        <Row className='mt-4'>
+          <Col/>
+          <Col>
+            <h2 style={{textAlign: 'center'}}>PickMaker™</h2>
+          </Col>
+          <Col style={{justifyContent: 'flex-end', display: 'flex'}}>
+            <Button size='lg' onClick={this.submitPicks} variant='primary'>Submit</Button>
+          </Col>
+        </Row>
+        <Row style={{justifyContent: 'center'}} className='mt-3'>
+          <Form.Group>
+            <Form.Label>Who are you?</Form.Label>
+            <Form.Control custom as='select' onChange={event => this.setState({name: event.target.value})} >
+              {this.state.players.map((player) => (
+                <option>{player}</option>
+              ))}
+            </Form.Control>
+          </Form.Group>
+        </Row>
+        <Row className='mt-3 mb-3'>
+          <Col>
+            <h4 style={{textAlign: 'center'}}>Pick game winners</h4>
+          </Col>
+          <Col>
+            <h4 style={{textAlign: 'center'}}>Sort picks by confidence</h4>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            {this.renderPickForm()}
+          </Col>
+          <Col>
+            {this.renderOrderer()}
+          </Col>
+        </Row>
+      </Container>
+    )
+  }
+
+  render() {
+    if (this.state.loadingGames) return null
+    return this.renderForm()
+  }
+}
+
+export default withRouter(PickMaker)
